82f05867ef21a79203897f89684144655623113d
rudycito23 <rrlucas@live.com>
2024-01-08 15:47:18 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/avoda.png"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  imports: [HomeComponent],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'home-app';
+  title = 'homes';
 }
