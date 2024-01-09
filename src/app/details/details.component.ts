4208978498509cfa52791f79898ca7e378747cec
rudycito23 <rrlucas@live.com>
2024-01-09 10:56:06 -0600

    Implement route parameters to pass data to a route; use routerLink directive to use dynamic data to create a route; use route parameter to retrieve data from the HousingService to display specific housing location information

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,62 @@
-import { Component } from '@angular/core';
+{/* Get route parameters from this component; update the code to display the id value 
+    passed using the route parameters. */}
+
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+
+{/* Next, update the template of the DetailsComponent to display the specific 
+    data represted by the housing location for the route parameter.
+
+    To access the data you will add a call to HousingService */}
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works!
-    </p>
+    <article>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+          <li>
+            Does this location have laundry: {{ housingLocation?.laundry }}
+          </li>
+        </ul>
+      </section>
+    </article>
   `,
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']); 
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+}
+
+{
+  /* the code below the export class, gives DetailsComponent access to the ActivatedRoute 
+    router feature that enables you to have access to the data about the current route. 
+    In the constructor, the code converts the id parameter acquired from the route 
+    from a string to a number. */
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -12,10 +12,19 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+{/* To use the routerLink directive, import RouterLink and RouterOutlet 
+    Line 34: add the routerLink directive to an anchor tag <a></a> 
+      the routerLink directive enables Angular's router to create dynamic links 
+      in the app. The value assigned to the routerLink is an array with two entries, 
+      the static path and the dynamic data; example below:
+      
+      [routerLink] = ['/path', dynamicData.id] */}
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
+  imports: [RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
@@ -27,6 +36,7 @@ import { HousingLocation } from '../housing-location';
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css'],
